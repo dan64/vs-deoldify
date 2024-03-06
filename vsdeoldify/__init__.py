fecfaf4f1dc176f7fb029458d32e45bdba96e1aa
Danilo <tillocad@libero.it>
2024-03-06 18:16:59 +0100

    Bump version to 1.1.5

--- vsdeoldify/__init__.py
+++ vsdeoldify/__init__.py
@@ -22,7 +22,7 @@ warnings.filterwarnings("ignore", category=FutureWarning, message="Arguments oth
 warnings.filterwarnings("ignore", category=UserWarning, message="Arguments other than a weight enum or `None`.*?")
 warnings.filterwarnings("ignore", category=UserWarning, message="torch.nn.utils.weight_norm is deprecated.*?")
 
-__version__ = "1.1.4"
+__version__ = "1.1.5"
 
 
 package_dir = os.path.dirname(os.path.realpath(__file__))
